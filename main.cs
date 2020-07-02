565e8109fd13cb6b52889fd6e9ed860a9d2ea052
Darko Donnie <donni3thedarko@gmail.com>
2020-07-02 00:19:13 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,54 @@
+using System;
+
+/* lecture 2: method override
+
+string showname() {
+  string name;
+  return name;
+}
+
+void showname(string name) {
+
+}
+
+*/
+
+/* Abstract class 
+project idea: https://1000projects.org/online-blood-bank-a-c-net-project.html */
+public abstract class Donator {
+
+  public abstract void listDescrition();
+}
+
+public class Seattle : Donator {
+
+  string name;
+  string type;
+
+  public override void listDescrition() {
+    Console.WriteLine("I do nothing");
+  }
+  
+
+}
+
+
+public class Toronto : Donator {
+
+  public override void listDescrition() {
+
+    DateTime d1 = new DateTime(2019, 6, 1, 17, 30, 0);
+    string last_donation = (d1.ToString()); // I do not know if I can do this
+    Console.WriteLine(last_donation);
+  }
+}
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+
+    Donator guy1 = new  Seattle();
+    Donator guy2 = new Toronto();
+    guy2.listDescrition();
+  }
+}
